7a42b61e25facb9eb9e4e4fc16789d0af6cc27e6
Minh-Demo <vuongminh211099@gmail.com>
2022-07-23 07:21:34 +0700

    web

--- include/slider.php
+++ include/slider.php
@@ -5,7 +5,7 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-	<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+	<!-- <li data-target="#carouselExampleIndicators" data-slide-to="3"></li> -->
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
@@ -19,8 +19,8 @@
     </div>
 	<!-- <div class="carousel-item">
       <img class="d-block w-100" src="https://khohangdienmay.vn/public/images/banner-tet.jpg" height="450" alt="Third slide">
-    </div>
-  </div> -->
+    </div> -->
+  </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
